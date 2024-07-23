50c75aa1eda98f2ec5adcbb02d08012e0d7245e5
c-ehrlich <ehrlich.christopher@gmail.com>
2024-07-23 18:53:06 +0200

    instrument openai
    more openai
    
    add events
    
    use vercel ai sdk
    
    update ai
    
    better prompt

--- src/server/api/routers/joke.ts
+++ src/server/api/routers/joke.ts
@@ -1,75 +1,46 @@
-import { TRPCError } from "@trpc/server";
+import { generateObject } from "ai";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { OpenAI } from "~/server/openai/openai";
+import { OpenAI } from "~/server/openai";
 
-function getJokePrompt(topic: string, makeJsonParseable: boolean) {
-  return `please write a joke about the following person or topic: ${topic}. use your knowledge of ${topic} and the world to make the joke relevant and funny.
-${
-  makeJsonParseable
-    ? `
-respond in the following format, which should be json parseable:
+function getJokePrompt(topic: string) {
+  return `Please write a joke about the following person or topic: ${topic}.
+Use your knowledge of ${topic} and the world to make the joke relevant and funny.
 
-\`\`\`json
+Respond in the following format, which should be json parseable:
 {
   "joke": "<the joke>",
   "whyFunny": "<why the joke is funny>"
-}
-\`\`\``
-    : ""
-}
-`;
+}`;
 }
 
 const jokeSchema = z.object({
   joke: z.string(),
   whyFunny: z.string(),
 });
-type JokeSchema = z.infer<typeof jokeSchema>;
 
 export const jokesRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ topic: z.string().min(1) }))
     .mutation(async ({ ctx, input }) => {
-      // TODO: use openai to generate a joke
-      const completion = await OpenAI.chat.completions.create({
-        model: "gpt-4o-mini-2024-07-18",
-        messages: [
-          {
-            role: "user",
-            content: getJokePrompt(input.topic, true),
+      const res = await generateObject({
+        model: OpenAI("gpt-4o-2024-05-13"),
+        prompt: getJokePrompt(input.topic),
+        schema: jokeSchema,
+        experimental_telemetry: {
+          isEnabled: true,
+          functionId: "create-joke",
+          metadata: {
+            metadataTest: "test",
           },
-        ],
+        },
       });
-
-      const res = completion.choices[0]?.message.content;
-
-      if (!res) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "OpenAI API returned no response",
-        });
-      }
-
-      let parsed: JokeSchema;
-      try {
-        const extractedJson =
-          (res.match(/\{[\s\S]*\}/) ?? [])[0] ?? "undefined";
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const jsonParsed = JSON.parse(extractedJson);
-        parsed = jokeSchema.parse(jsonParsed);
-      } catch (e) {
-        throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "OpenAI API returned invalid response",
-        });
-      }
 
       await ctx.db.joke.create({
         data: {
           topic: input.topic,
-          ...parsed,
+          ...res.object,
         },
       });
     }),
--- src/server/openai/openai.ts
+++
@@ -1,8 +0,0 @@
-import OpenAISDK from "openai";
-import { env } from "~/env";
-
-export const OpenAI = new OpenAISDK({
-  organization: env.OPENAI_ORG,
-  project: env.OPENAI_PROJECT_ID,
-  apiKey: env.OPENAI_API_KEY,
-});
